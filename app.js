95c5bcbdaa87f5db377bb9347f21e428fd1eeeea
SaddamMohsen <sad.2000@outlook.com>
2019-12-16 14:21:49 +0300

    first commit of blog with users

--- app.js
+++ app.js
@@ -5,25 +5,26 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const middleware = require('./utils/middleware')
-//const usersRouter = require('./controllers/users')
-const blogsRouter=require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
 
 
-logger.info('connected to',config.MONGODB_URI)
+logger.info('connected to', config.MONGODB_URI)
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true ,useUnifiedTopology: true})
-         .then(result => {   
-          logger.info('connected to MongoDB')})
-		  .catch((error) => {   
-  		     logger.error('error connecting to MongoDB:', error.message)
-			 }) 
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB:', error.message)
+    })
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(middleware.requestLogger)
-app.use('/api/blogs',blogsRouter)
-//app.use('/api/users',usersRouter)
+app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
-module.exports=app
+module.exports = app
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,46 +1,56 @@
-
 const blogsRouter = require('express').Router()
-const Blog= require('../models/Blog')
+const Blog = require('../models/Blog')
+const User = require('../models/User')
 
-blogsRouter.get('/',async(request,response) => {
-    console.log('from get')
-  const blogs= await Blog.find({})
-    
-      response.json(blogs.map(blog =>blog.toJSON()))
+blogsRouter.get('/', async(request, response) => {
+    //console.log('from get')
+    const blogs = await Blog
+    .find({}).populate('user',{username:1,name:1})
+    response.json(blogs.map(blog => blog.toJSON()))
 })
 
-blogsRouter.post('/', async(request, response,next) => {
-  
-  const blogg = new Blog(request.body)
-   try{
-      const blogSaved = await blogg.save()
-     response.json(blogSaved.toJSON())
-   }catch(err){next(err)}
+blogsRouter.post('/', async(request, response, next) => {
+    const body = request.body
+    const user = await User.findById(body.userId)
+    const blogg = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+        user: user._id
+    })
+    try {
+        const blogSaved = await blogg.save()
+        user.blogs = user.blogs.concat(blogSaved._id)
+        await user.save()
+        response.json(blogSaved.toJSON())
+    } catch (err) {
+        next(err)
+    }
 })
-blogsRouter.delete('/:id',async(req,resp,next)=>{
-   try{
-     await Blog.findByIdAndRemove(req.params.id)
-     resp.status(204).end()
-  }catch (exception) {
-    next(exception)
-  }
+blogsRouter.delete('/:id', async(req, resp, next) => {
+    try {
+        await Blog.findByIdAndRemove(req.params.id)
+        resp.status(204).end()
+    } catch (exception) {
+        next(exception)
+    }
 })
-blogsRouter.put('/:id',async(req,resp,next)=>{
-  const body= req.body
-  const blog={
-    title:body.title,
-    author:body.author,
-    url:body.url,
-    likes:body.likes
-  }
-  console.log('from put')
-  try{
-  const updateblog=await Blog.findByIdAndUpdate(req.params.id,blog,{new:true})
-         console.log('inside ',updateblog)
-         resp.json(updateblog.toJSON())
+blogsRouter.put('/:id', async(req, resp, next) => {
+    const body = req.body
+    const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    }
+    console.log('from put')
+    try {
+        const updateblog = await Blog.findByIdAndUpdate(req.params.id, blog, { new: true })
+        console.log('inside ', updateblog)
+        resp.json(updateblog.toJSON())
 
-  }catch(excep)
-     {next(excep)}
+    } catch (excep) { next(excep) }
 })
 
 module.exports = blogsRouter
